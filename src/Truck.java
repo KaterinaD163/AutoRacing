b9e0cee133c27bc74ade33ae71f5694e2649613e
KaterinaD163 <katja_163@mail.ru>
2022-10-11 23:16:05 +0400

    Дз №1, №2, №3, №4 с исправлениями + первая часть домашки

--- src/Truck.java
+++ src/Truck.java
@@ -1,9 +1,14 @@
 public class Truck extends Transport implements Competing {
+    public enum loadTonnage {first, second, third}
+
 
     public Truck(String brand, String model, float engineVolume) {
         super(brand, model, engineVolume);
+
     }
 
+    public void printLoadTonnage() {
+    }
     public void printTruck() {
         System.out.println(getBrand() + " " + getModel() + ", объём двигателя " + getEngineVolume() + " л ");
     }
@@ -20,22 +25,26 @@ public class Truck extends Transport implements Competing {
 
     @Override
     public String toString() {
+        final String first = " №1 (с полной массой до 3,5 тонн)";
+        final String second = " №2 с полной массой свыше 3,5 до 12 тонн)";
+        final String third = " №3 (с полной массой свыше 12 тонн)";
+
         return "Truck{}";
     }
 
 
-    public static void pitStop() {
+    public void pitStop() {
         System.out.println(" Здесь был пит-стоп");
 
     }
 
 
-    public static void bestRoundTime() {
+    public void bestRoundTime() {
         System.out.println(" Лучшее время круга - 6 минут");
     }
 
 
-    public static void maxSpeed() {
+    public void maxSpeed() {
         System.out.println(" Максимальная скорость - 170 км/ч");
 
     }
